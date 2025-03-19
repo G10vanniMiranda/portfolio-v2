15db8e92e8e01db960b4c8fc7ba907acca31aef0
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-03-18 22:46:56 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,102 @@
 import Image from "next/image";
+import Link from "next/link";
+import { AiFillProject } from "react-icons/ai";
+import { CiLinkedin } from "react-icons/ci";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { IoIosHome } from "react-icons/io";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-screen h-screen flex flex-col">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <section className="w-full h-auto flex flex-col items-center g-3">
+
+        {/* Header */}
+        <div className=" w-full h-24 flex justify-center items-center gap-3">
+
+          <div className="bg-green-500 flex items-center justify-end p-2 gap-3 w-32 h-8 rounded-l-md">
+            <h2>Home</h2>
+            <p>|</p>
+            <IoIosHome className="text-black" />
+          </div>
+
+          <div className="bg-green-500 flex items-center justify-start p-2 gap-3 w-32 h-8 rounded-e-md">
+            <AiFillProject className="text-black" />
+            <p>|</p>
+            <h2>Projetos</h2>
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Foto de Perfil */}
+        <div className="w-full h-[400px] flex justify-center" >
+          <Image width={200} height={100} src="/perfil.png" alt="Foto de Perfil" className="w-96 rounded-2xl" />
+        </div>
+
+        {/* Sobre Mim */}
+        <div className="w-full flex flex-col justify-center items-center gap-2 pt-3" >
+
+          <div className="w-96 flex justify-start">
+            <h2 className="text-green-500">Olá, meu nome é</h2>
+          </div>
+
+          <div className="w-96 mb-3">
+            <h2 className="text-4xl">Giovanni Miranda</h2>
+          </div>
+
+          <div className="w-96 h-auto">
+            <h2>
+              Olá, tenho 28 anos e sou de Porto Velho, RO. Estou cursando o 5º período de Ciências
+              da Computação e trabalho há 2 anos na área de suporte técnico, com sólida experiência
+              em Help Desk. Sou apaixonado por tecnologia e busco constantemente me aperfeiçoar na
+              área de desenvolvimento de software. Meu objetivo é me tornar um desenvolvedor full stack,
+              e estou trabalhando em projetos que refletem meu aprendizado e crescimento profissional.
+            </h2>
+          </div>
+
+        </div>
+
+        {/* Tecnologias */}
+        <div className=" w-full flex flex-col gap-2 items-center pt-6">
+
+          <div className="w-96 flex gap-2">
+            <div className="bg-green-800 text-green-300 rounded-md px-2">Next.js</div>
+            <div className="bg-green-800 text-green-300 rounded-md px-2">Next Auth</div>
+            <div className="bg-green-800 text-green-300 rounded-md px-2">TailWindCSS</div>
+            <div className="bg-green-800 text-green-300 rounded-md px-2">Bootstrap</div>
+          </div>
+
+          <div className="w-96 flex gap-2">
+            <div className="bg-green-800 text-green-300 rounded-md px-2">TypeScript</div>
+            <div className="bg-green-800 text-green-300 rounded-md px-2">Prisma</div>
+            <div className="bg-green-800 text-green-300 rounded-md px-2">React</div>
+            <div className="bg-green-800 text-green-300 rounded-md px-2">JSON</div>
+            <div className="bg-green-800 text-green-300 rounded-md px-2">API</div>
+          </div>
+
+        </div>
+
+        {/* Contato */}
+        <div className="w-96 flex items-center justify-items-start gap-3 pt-8">
+
+          <Link href="https://api.whatsapp.com/send?phone=5569999210459">
+            <div className="bg-green-500 rounded-md p-2 p flex gap-2">
+              Entre em contato --{">"}
+            </div>
+          </Link>
+
+          <div className="flex gap-2">
+            <FiGithub className="cursor-pointer" />
+            <CiLinkedin className="cursor-pointer" />
+            <FaInstagram className="cursor-pointer" />
+            <FaWhatsapp className="cursor-pointer" />
+          </div>
+
+        </div>
+
+      </section>
+
     </div>
   );
 }
